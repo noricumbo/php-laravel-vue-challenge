227a536ae2cfd8dae6da2094d970fb3e13c8ac42
Jorge Noricumbo <noricumbo@gmail.com>
2025-05-05 17:08:31 -0600

    Create Nav Vue component

---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav>
+        <ul class=" flex space-x-6 list-inside">
+            <li>
+                <NavLink href="/" :active="$page.component === 'Home'">
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink href="/users" :active="$page.component === 'Users/Index'">
+                    Users
+                </NavLink>
+            </li>
+            <li>
+                <NavLink href="/teams" :active="$page.component === 'Teams/Index'">
+                    Teams
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+import NavLink from "./NavLink.vue"
+</script>
