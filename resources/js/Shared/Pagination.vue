f42b372416d8a130ffafb5b2aba62483fe94b60e
Jorge Noricumbo <noricumbo@gmail.com>
2025-05-05 17:30:07 -0600

    Create Pagination reusable component for Vue templates

---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex justify-center">
+        <Component
+            :is="link.url ? 'Link' : 'span'"
+            v-for="link in links"
+            :href="link.url"
+            v-html="link.label"
+            class="px-1 text-blue-400 hover:text-blue-600"
+            :class="{ 'text-gray-400 hover:text-gray-400' : !link.url, 'font-bold text-gray-800' : link.active }"
+        />
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    links: Array
+})
+</script>
